c72db297d80f2ab2e70e6e305acf240f8684964f
Timur <v.timur8484@gmail.com>
2018-07-22 23:28:07 +0300

    Оформить заказ тест

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -14,13 +14,13 @@ class AppAsset extends AssetBundle
         'lib/fontawesome/css/all.css',
         'lib/bxslider/dist/jquery.bxslider.min.css',
         'lib/fancybox/dist/jquery.fancybox.css',
-        'css/main.css?v=128',
+        'css/main.css?v=129',
     ];
     public $js = [
         'lib/bxslider/dist/jquery.bxslider.min.js',
         'lib/fancybox/dist/jquery.fancybox.js',
         'lib/jquery.maskedinput.min.js',
-        'js/main.js?v=128'
+        'js/main.js?v=129'
     ];
     public $depends = [
         // 'yii\web\YiiAsset',
