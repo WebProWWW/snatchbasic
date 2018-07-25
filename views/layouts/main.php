e48d317b15e925e9b6554fa9659a2161c9f46642
Timur <v.timur8484@gmail.com>
2018-07-25 15:03:05 +0300

    GTM земена

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -35,13 +35,15 @@ AppAsset::register($this);
   <meta name="msapplication-TileColor" content="#FFFFFF">
   <meta name="msapplication-TileImage" content="<?= $urlBase ?>/mstile-144x144.png">
 
+  <meta name="google-site-verification" content="sAnRQLdBC6ycJ2ha0YJsFubXzabQT-EOZhGt79YWssE">
+
   <?php if (!YII_ENV_DEV): ?>
   <!-- Google Tag Manager -->
   <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-W3JGB3W');</script>
+  })(window,document,'script','dataLayer','GTM-WDKR4PS');</script>
   <!-- End Google Tag Manager -->
   <?php endif ?>
 
@@ -50,7 +52,7 @@ AppAsset::register($this);
 
 <?php if (!YII_ENV_DEV): ?>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W3JGB3W"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDKR4PS"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 <?php endif ?>
